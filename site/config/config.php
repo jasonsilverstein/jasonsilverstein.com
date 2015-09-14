6f7e83e9913227f8752a7c49cc0d99ad979dee57
Jason Silverstein <jason@jasonsilverstein.com>
2015-09-14 13:09:20 -0700

    License Key
    license key

--- site/config/config.php
+++ site/config/config.php
@@ -15,7 +15,7 @@ for more information: http://getkirby.com/license
 
 */
 
-c::set('license', 'put your license key here');
+c::set('license', 'K2-PERSONAL-ae573be97011e94e1098b181766ba5ef');
 
 /*
 
